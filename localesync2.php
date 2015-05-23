33fb66b091fc191ce31aae8867f0d6645092549c
semteacher <semteacher@gmail.com>
2015-05-23 19:36:02 +0300

    look like localesync2.php is working now....

--- localesync2.php
+++ localesync2.php
@@ -12,7 +12,7 @@ body {background:White;}
 </head>
 <body>
 <h2>OJS-OCS XML Locale Data Values Sync</h2>
-<form action="localesync.php" method="post" enctype="multipart/form-data">
+<form action="localesync2.php" method="post" enctype="multipart/form-data">
     <div>
     Select source file to sync:
     <input type="file" name="sourcefile" id="sourcefile">
@@ -28,51 +28,95 @@ body {background:White;}
 
 <?php
 echo 'ready';
-
+//check is the form was submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-var_dump($_POST);
-var_dump($_FILES);
+    var_dump($_POST);
+    var_dump($_FILES);
 
-$xmlsource=simplexml_load_file($_FILES['sourcefile']['tmp_name']) or die("Error: Cannot open source object");
-$xmldest=simplexml_load_file($_FILES['destfile']['tmp_name']) or die("Error: Cannot open destination object");
-print_r($xmlsource->message[1]['key']);
-//print_r($xmlsource->message[1]);
-print_r("<br>====================================<br>");
-    foreach ($xmlsource->message as $xmlsourceinfo):
-        $mkey= (string) $xmlsourceinfo['key'];
-        $mtext= (string) $xmlsourceinfo;
-        print_r($mkey);
-        print_r("<br>");
-        print_r($mtext);
-        //var_dump($key);
-        //var_dump($xmlsourceinfo);
-        //echo '<li><div class="key">'$key'</div><div class="text">'$text'</li>n';
-        print_r("<br>:::::::::::::::::::::::::::::::::::::<br>"); 
-        $myDataObjects = $xmldest->xpath('/locale/message[@key="'.$mkey.'"]');
+   // $xmlsource=simplexml_load_file($_FILES['sourcefile']['tmp_name']) or die("Error: Cannot open source object");
+   // $xmldest=simplexml_load_file($_FILES['destfile']['tmp_name']) or die("Error: Cannot open destination object");
+    //open source file
+    $domsource = new DOMDocument('1.0');
+    $domsource->load($_FILES['sourcefile']['tmp_name']);
+    //open destination
+    $domdest = new DOMDocument('1.0');
+    $domdest->load($_FILES['destfile']['tmp_name']);
+    //$domsource->childNodes->item(0)->replaceChild() attributes-> getNamedItem()
+    //print $domsource->saveXML();
+    //get nodelist by tag name
+    $sourcenodes = $domsource->getElementsByTagName('message');
+    $destnodes = $domdest->getElementsByTagName('message');
+    var_dump($destnodes);
+    //process nodelist
+    foreach ($sourcenodes as $nodecontent) {
+        if ($nodecontent->hasAttributes()) {
+            $localekeyvalue = $nodecontent->attributes->getNamedItem('key');
+            $localekeyval = $nodecontent->getAttribute('key');
+            //process just nodes with existed 'key' attribute
+            if (!is_null($localekeyval)) {
+                var_dump($nodecontent->nodeValue);
+                //foreach ($destnodes as $destnodecontent) {
+                //    if ($destnodecontent->getAttribute('key') == $localekeyval)
+                //    {
+                //        var_dump($destnodecontent);
+                //        echo "---founded-00-<br>";
+                //        $destnodecontent->parentNode->replaceChild($nodecontent, $destnodecontent);
+                //    }
+                //}
+                $i = $destnodes->length - 1;
+                while ($i > -1) {
+                    $destnodecontent = $destnodes->item($i);
+                    if ($destnodecontent->getAttribute('key') == $localekeyval) {
+                        echo "---founded-00-<br>";
+                        $nodecontent = $domdest->importNode($nodecontent, true);
+                        //$newelement = $domdest->createTextNode('Some new node!');
+                        //$destnodecontent->parentNode->replaceChild($newelement, $destnodecontent);
+                        $destnodecontent->parentNode->replaceChild($nodecontent, $destnodecontent);
+                    }
 
-        if ($myDataObjects!=FALSE) {
-                var_dump($myDataObjects);
-        $mkeydest= (string) $myDataObjects[0]['key'];
-        $mtextdest= (string) $myDataObjects[0];
-        print_r($mkeydest);
-        print_r("<br>");
-        print_r($mtextdest);
-        $repmyDataObject = $myDataObjects[0];
-        //$repmyDataObject{0}=$mtext;
-        $repmyDataObject{0}= $xmlsourceinfo;
-        print_r("<br>");
-        $mtextdest= (string) $myDataObjects[0];
-        print_r($mtextdest);
+                    $i--;
+                }
+                    $text = $nodecontent->firstChild->nodeValue;
+                //$title = $video->getElementsByTagName('title')->item(0)->nodeValue;
+                //$id    = ??
+                //$thumb = ??
+                var_dump($nodecontent->attributes);
+                //print_r($nodecontent->attributes);
+                $length = $nodecontent->attributes->length;
+                $name = $nodecontent->attributes->item(0)->name;
+
+                echo "nodecontent: attribute `$name`<br>";
+                echo "nodecontent: attribute `$localekeyval`<br>";
+                echo "nodecontent: attribute length `$length`<br>";
+                //for ($i = 0; $i < $length; ++$i) {
+                //    $name = $nodecontent->attributes->item($i)->name;
+                //    $value = $nodecontent->attributes->item($i)->value;
+                //$h1->removeAttribute($name);
+                //    echo "nodecontent: attribute `$name`<br>";
+                //    echo "nodecontent: attribute `$value`<br>";
+                //echo "nodecontent: attribute `$nodecontent->attributes->item($i)->nodeValue`<br>";
+                //}
+                //print($nodecontent->attributes);
+                print_r('<br>1;;;;;;;;;;;;<br>');
+                var_dump($nodecontent->firstChild);
+                print_r('<br>2;;;;;;;;;;;;<br>');
+                var_dump($text);
+                print_r('<br>3;;;;;;;;;;;;<br>');
+                print_r($text);
+                print_r('<br>/////<br>');
+            }
         }
-        print_r("<br>-------------------------------<br>");        
-    endforeach;
-print_r("<br>=====================================<br>");    
-var_dump($xmldest);
-  $dom = new DOMDocument('1.0');
-  $dom->preserveWhiteSpace = false;
-  $dom->formatOutput = true;
-  $dom->loadXML($xmldest->asXML());
-  echo $dom->save($_FILES['destfile']['name']);
+    }
+
+    echo $domdest->save($_FILES['destfile']['name']);
+
+
+//  $dom = new DOMDocument('1.0');
+//  $dom->preserveWhiteSpace = false;
+//  $dom->formatOutput = true;
+//  $dom->loadXML($xmldest->asXML());
+//  echo $dom->save($_FILES['destfile']['name']);
+
 //$xmldest->asXML($_FILES['destfile']['name']);
 }
 ?>
